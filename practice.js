ca86d4ba51ce37f5155c8f6f1cdfa745649f973e
aryanpatel0318 <99696854+aryanpatel0318@users.noreply.github.com>
2023-12-16 12:55:04 +0530

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,90 @@
+//09.1 - 09.2
+
+/* function setup() {
+    createCanvas(windowWidth,windowHeight);
+
+}
+
+function draw(){
+    if (mouseIsPressed) {
+        ellipse(mouseX, mouseY, 50, 50);
+    }
+}
+
+*/
+
+
+ /* function setup() {
+    createCanvas(windowWidth,windowHeight);
+
+}
+
+function draw(){
+    if (keyIsPressed) {
+        ellipse(random(width), random(height), 50);
+    }
+}
+
+*/
+
+
+/* function setup() {
+    createCanvas(windowWidth,windowHeight);
+
+}
+
+function draw(){
+    if (!mouseIsPressed) {
+        ellipse(mouseX, mouseY, 50, 50);
+    }
+}
+
+*/
+
+
+/* function setup() {
+    createCanvas(windowWidth,windowHeight);
+
+}
+
+function draw(){
+    if (!keyIsPressed) {
+        ellipse(random(width), random(height), 50);
+    }
+}
+
+*/
+
+//09.3
+
+
+/* function setup() {
+    createCanvas(windowWidth,windowHeight);
+}
+
+function mousePressed () {
+    if (mouseButton == LEFT) {
+        print("you did a left click.");
+    } if (mouseButton == RIGHT) {
+        print("you did a right click");
+    } if (mouseButton == CENTER) {
+        print("you did a middle click");
+    }
+}
+
+*/
+
+//10.1
+
+/* const WEIGHT = 10;
+
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+}
+
+function draw () {
+    strokeWeight(WEIGHT);
+    ellipse(mouseX,mouseY,50);
+}
+
+*/
